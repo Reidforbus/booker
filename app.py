606084f3ec5e2dbc135ab06c43e4250b973117ad
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-09 17:37:52 +0300

    Added pip reqs and began trialing with login

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask
+from flask import redirect, render_template, request, session
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html", motd="Book your time here")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    user = request.form["email"]
+    # password = request.form["pwd"]
+    # TODO: validate credentials
+    session["user"] = user
+    return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    del session["user"]
+    return redirect("/")

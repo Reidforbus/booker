c48fe675b250f95a977546b528ad93439041b7c2
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-11 01:26:50 +0300

    Added template and routing for register page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 
 @app.route("/")
@@ -11,16 +12,35 @@ def index():
     return render_template("index.html", motd="Book your time here")
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET"])
 def login():
-    user = request.form["email"]
+    return redirect("/")
+
+
+@app.route("/login", methods=["POST"])
+def handlelogin():
+    username = request.form["username"]
     # password = request.form["pwd"]
     # TODO: validate credentials
-    session["user"] = user
+    session["username"] = username
     return redirect("/")
 
 
 @app.route("/logout")
 def logout():
-    del session["user"]
+    del session["username"]
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/register", methods=["POST"])
+def handleregister():
+    name = request.form["name"]
+    username = request.form["username"]
+    pwd = request.form["pwd"]
+    session["username"] = username
     return redirect("/")

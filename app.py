19f157e668272a19bc0145ba7e9ade5f81330757
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-14 00:21:48 +0300

    moved to using base layout and added css

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, url_for
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
@@ -14,27 +14,29 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route("/")
-def index(motd="Book your time here"):
-    return render_template("index.html", motd=motd)
+def index():
+    return render_template("index.html")
 
 
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return redirect("/")
-
-
-@app.route("/login", methods=["POST"])
-def handlelogin():
+    if request.method == "GET":
+        return redirect("/")
     username = request.form["username"]
     # password = request.form["pwd"]
     # TODO: validate credentials
-    session["username"] = username
+    query = text("SELECT admin FROM users WHERE username=:username")
+    result = db.session.execute(query, {"username": username}).fetchone()
+    if result is not None:
+        session["username"] = username
+        session["admin"] = result.admin
     return redirect("/")
 
 
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["admin"]
     return redirect("/")
 
 
@@ -46,14 +48,27 @@ def register():
 @app.route("/register", methods=["POST"])
 def handleregister():
     username = request.form["username"]
+    if username == "":
+        return redirect("/error")
     sameusrnm = text("SELECT COUNT(*) FROM users WHERE username=:username")
     result = db.session.execute(sameusrnm, {"username": username}).fetchone()
     app.logger.debug(result[0])
     if result[0] == 1:
-        return redirect("/register")
+        return redirect("/error")
     name = request.form["name"]
     pwd_hash = generate_password_hash(request.form["pwd"])
     sqlinsert = "INSERT INTO users (username, pwd, name) VALUES (:username, :pwd, :name)"
     db.session.execute(text(sqlinsert), {"username":username, "pwd":pwd_hash, "name":name})
     db.session.commit()
     return redirect("/")
+
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
+
+
+def is_admin():
+    if session["username"]:
+        return session["admin"]
+    return False

97448bebf8e081954b534f23645c68ffed6cc642
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-17 18:32:51 +0300

    started moving db functionality to out of route logic

--- db.py
+++ db.py
@@ -1,6 +1,13 @@
 from app import app
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
+
+
+def validate_login(username, pwd):
+    # TODO validate login with password
+    query = text("SELECT admin FROM users WHERE username=:username")
+    return db.session.execute(query, {"username": username}).fetchone()

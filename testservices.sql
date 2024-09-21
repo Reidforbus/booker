7b53aacba284c91c8d5047651eff10cb223dc268
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-21 15:58:59 +0300

    added test data for services

---
+++ testservices.sql
@@ -0,0 +1,6 @@
+INSERT INTO service_items (name, description, dur, price) VALUES ('Tire change', 'Includes the changing of one tire and/or inner tube. Tubeless install for +10€', 15, 15);
+INSERT INTO service_items (name, description, dur, price) VALUES ('First time service', 'Includes gear adjustment, wheel truing and brake adjustment. Wash for +10€', 60, 60);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 2', 'Description of service', 60, 100);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 3', 'Description of service', 60, 100);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 4', 'Description of service', 60, 100);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 5', 'Description of service', 60, 100);

85ebe226251329f89709ea1f075e227d93550dea
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-30 20:38:43 +0300

    sample durations to intervals

--- testservices.sql
+++ testservices.sql
@@ -1,6 +1,6 @@
-INSERT INTO service_items (name, description, dur, price) VALUES ('Tire change', 'Includes the changing of one tire and/or inner tube. Tubeless install for +10€', 15, 15);
-INSERT INTO service_items (name, description, dur, price) VALUES ('First time service', 'Includes gear adjustment, wheel truing and brake adjustment. Wash for +10€', 60, 60);
-INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 2', 'Description of service', 60, 100);
-INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 3', 'Description of service', 60, 100);
-INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 4', 'Description of service', 60, 100);
-INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 5', 'Description of service', 60, 100);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Tire change', 'Includes the changing of one tire and/or inner tube. Tubeless install for +10€', '20 minutes', 15);
+INSERT INTO service_items (name, description, dur, price) VALUES ('First time service', 'Includes gear adjustment, wheel truing and brake adjustment. Wash for +10€', '60 minutes', 60);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 2', 'Description of service', '60 minutes', 100);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 3', 'Description of service', '40 minutes', 50);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 4', 'Description of service', '2 hours', 200);
+INSERT INTO service_items (name, description, dur, price) VALUES ('Sample service 4', 'Description of service', '1 hour', 100);

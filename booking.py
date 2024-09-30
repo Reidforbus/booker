89b8f846ac63c7a7e77b2106b9c6296a5eeed977
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-30 20:02:23 +0300

    booking date is given in query string with iso format

--- booking.py
+++ booking.py
@@ -1,15 +1,27 @@
 from flask import session, render_template
 import db
+import datetime
 
 
-def get_booking(req, id, week=None):
+def get_booking(req, id):
     if "username" not in session:
         return render_template("error.html", errmsg="You must be logged in to book services")
     service = db.get_service(id)
     if not service:
         return render_template("error.html", errmsg="Could not find service")
-    if not week:
-        week = 1
+    date = req.args.get("date")
+    if not date:
+        date = datetime.date.today()
+    else:
+        try:
+            date = datetime.date.fromisoformat(date)
+        except ValueError:
+            return render_template("error.html", errmsg="Date is not valid")
+    monday = date - datetime.timedelta(days=date.weekday())
+    return render_template("booking.html", product=service, dayslots=sampletimes(), week=date.isocalendar()[1])
+
+
+def sampletimes():
     days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
     times = []
     for day in days:
@@ -17,4 +29,4 @@ def get_booking(req, id, week=None):
         time["weekday"] = day
         time["slots"] = [1, 2, 3, 4]
         times.append(time)
-    return render_template("booking.html", product=service, dayslots=times, week=week)
+    return times

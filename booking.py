e8f3171623ff4f7407d57eca9ff2d5d1d37a8ede
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-30 21:10:24 +0300

    small changes

--- booking.py
+++ booking.py
@@ -6,9 +6,11 @@ import datetime
 def get_booking(req, id):
     if "username" not in session:
         return render_template("error.html", errmsg="You must be logged in to book services")
+
     service = db.get_service(id)
     if not service:
         return render_template("error.html", errmsg="Could not find service")
+
     date = req.args.get("date")
     if not date:
         date = datetime.date.today()
@@ -18,6 +20,8 @@ def get_booking(req, id):
         except ValueError:
             return render_template("error.html", errmsg="Date is not valid")
     monday = date - datetime.timedelta(days=date.weekday())
+
+    free_slots = get_free_slots(monday, service[3])
     return render_template("booking.html", product=service, dayslots=sampletimes(), week=date.isocalendar()[1])
 
 
@@ -30,3 +34,7 @@ def sampletimes():
         time["slots"] = [1, 2, 3, 4]
         times.append(time)
     return times
+
+
+def get_free_slots(start_date, duration):
+    print(start_date, duration)

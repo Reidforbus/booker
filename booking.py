1722b683e09f700d2234f6877eb37833f28e4731
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-09-30 16:25:00 +0300

    Added code and styling for showing available timeslots

--- booking.py
+++ booking.py
@@ -3,9 +3,16 @@ import db
 
 
 def get_booking(req, id):
-    if "username" in session:
-        service = db.get_service(id)
-        days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-        return render_template("booking.html", product=service, days=days)
-    else:
+    if "username" not in session:
         return render_template("error.html", errmsg="You must be logged in to book services")
+    service = db.get_service(id)
+    if not service:
+        return render_template("error.html", errmsg="Could not find service")
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    times = []
+    for day in days:
+        time = {}
+        time["weekday"] = day
+        time["slots"] = [1, 2, 3, 4]
+        times.append(time)
+    return render_template("booking.html", product=service, times=times)

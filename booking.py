d17cd4199459b5672ecf5c8bcb5afdd7502b38fc
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-10-03 23:47:31 +0300

    free slots for the week

--- booking.py
+++ booking.py
@@ -27,9 +27,9 @@ def get_booking(req, id):
 
 
 def sampletimes():
-    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
     times = []
-    for day in days:
+    for day in weekdays:
         time = {}
         time["weekday"] = day
         time["slots"] = [1, 2, 3, 4]
@@ -38,14 +38,36 @@ def sampletimes():
 
 
 def get_free_slots(start_date: datetime.date, duration: datetime.timedelta):
-    print(start_date, duration)
+    weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
     dayslots = []
     for offset in range(7):
         day = {}
         date = start_date + datetime.timedelta(days=offset)
+        day["weekday"] = weekdays[date.weekday()]
+        day["slots"] = []
         opening_hours = db.get_hours(date)
+        if not opening_hours:
+            dayslots.append(day)
+            continue
         taken_times = db.get_bookings(date)
         print("Day:", offset, opening_hours, taken_times)
 
+        block_size = datetime.timedelta(minutes=20)
+        start = datetime.datetime.combine(date, opening_hours[0])
+        close = datetime.datetime.combine(date, opening_hours[1])
+        j = 0
+
+        while start + duration <= close:
+            end = start + duration
+            if len(taken_times) > 0 and j < len(taken_times):
+                if end.time() <= taken_times[j][2]:
+                    day["slots"].append((start.time(), end.time()))
+                else:
+                    start = datetime.datetime.combine(date, taken_times[j][2]) + taken_times[j][1]
+                    j += 1
+                    continue
+            else:
+                day["slots"].append((start.time(), end.time()))
+            start += block_size
         dayslots.append(day)
     return dayslots
